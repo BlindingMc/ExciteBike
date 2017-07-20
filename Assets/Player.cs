2bfcba84d207cfa2806b297e44c347a54d0bc20c
Michael Collier <michaelcollier4242@gmail.com>
2017-07-19 23:45:06 -0400

    Getting Started
    Made prototype area, starting to figure out how to define each tire as array of rigidbody.., this is to go up and down ramps properly

---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Player : MonoBehaviour {
+	private WheelCollider[] m_WheelColliders = new WheelCollider[2];
+	private GameObject[] m_WheelMeshes = new GameObject[2];
+	private float m_TopSpeed = 15.0f;
+
+
+
+	CharacterController m_cc;
+	Rigidbody m_rb;
+
+
+
+
+	private float rotateSpeed = 5.0f;
+	private float speed = 5.0f;
+	private float speedB = 7.0f;
+	Vector3 moveDirection = Vector3.zero;
+
+	public float CurrentSpeed{ get { return m_rb.velocity.magnitude*2.23693629f; }}
+	public float MaxSpeed{get { return m_TopSpeed; }};
+
+	// Use this for initialization
+	void Start () {
+		m_cc = GetComponent<CharacterController> ();
+		m_rb = GetComponent<Rigidbody>();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate (0, 0, Input.GetAxis ("Vertical") * -rotateSpeed);
+		float curSpeed = speed * Input.GetAxis ("Horizontal");
+		cc.SimpleMove (transform.right * -curSpeed);
+
+	
+	}
+}
